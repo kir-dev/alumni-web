f5cebbcc7ad406cb102357d1d58ee30c76a20530
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-09-21 12:30:08 +0200

    feat: plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,12 @@ import './globals.css';
 
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 import React from 'react';
 
 import { NavbarWrapper } from '@/components/navbar/navbar-wrapper';
 import { Footer } from '@/components/ui/footer';
+import { ANALYTICS_URL } from '@/config/environment.config';
 import { getDomainForHost } from '@/lib/server-utils';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -24,6 +26,9 @@ export default async function RootLayout({
 
   return (
     <html lang='hu'>
+      <Head>
+        {ANALYTICS_URL && <script defer data-domain={ANALYTICS_URL} src='https://visit.kir-dev.hu/js/script.js' />}
+      </Head>
       <body className={inter.className}>
         <NavbarWrapper group={domain?.group} />
         {children}
--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -25,3 +25,5 @@ export const VERCEL_TEAM_ID = env.get('VERCEL_TEAM_ID').required().asString();
 export const VERCEL_API_TOKEN = env.get('VERCEL_API_TOKEN').required().asString();
 
 export const EMAIL_DISABLE = env.get('EMAIL_DISABLE').default('false').asBool();
+
+export const ANALYTICS_URL = env.get('ANALYTICS_URL').asString();
