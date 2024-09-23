d5b6b960e07a5b347cb54b930dfa9d14d891cf79
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-09-24 00:24:10 +0200

    feat: kir-mail support

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -7,12 +7,9 @@ export const SITE_URL = env.get('SITE_URL').required().asString();
 export const NEXTAUTH_SECRET = env.get('NEXTAUTH_SECRET').required().asString();
 
 export const EMAIL_HOST = env.get('EMAIL_HOST').required().asString();
-export const EMAIL_PORT = env.get('EMAIL_PORT').required().asPortNumber();
-export const EMAIL_SECURE = env.get('EMAIL_SECURE').required().asBool();
-export const EMAIL_USERNAME = env.get('EMAIL_USERNAME').required().asString();
-export const EMAIL_PASSWORD = env.get('EMAIL_PASSWORD').required().asString();
+export const EMAIL_API_KEY = env.get('EMAIL_API_KEY').required().asString();
 export const EMAIL_FROM_NAME = env.get('EMAIL_FROM_NAME').required().asString();
-export const EMAIL_FROM_ADDRESS = env.get('EMAIL_FROM_ADDRESS').required().asString();
+export const EMAIL_QUEUE = env.get('EMAIL_QUEUE').required().asString();
 export const EMAIL_REPLY_TO = env.get('EMAIL_REPLY_TO').asString();
 
 env
--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,22 +1,59 @@
+import axios from 'axios';
 import { SendMailOptions } from 'nodemailer';
 
-import { EmailTransport } from '@/config/email.config';
-import { EMAIL_DISABLE } from '@/config/environment.config';
+import {
+  EMAIL_API_KEY,
+  EMAIL_DISABLE,
+  EMAIL_FROM_NAME,
+  EMAIL_HOST,
+  EMAIL_QUEUE,
+  EMAIL_REPLY_TO,
+} from '@/config/environment.config';
+
+const mailingAxios = axios.create({
+  baseURL: EMAIL_HOST,
+  headers: {
+    Authorization: `Api-Key ${EMAIL_API_KEY}`,
+  },
+});
 
 export async function singleSendEmail(options: SendMailOptions) {
   if (EMAIL_DISABLE) return;
   try {
-    await EmailTransport.sendMail(options);
+    await mailingAxios.post('/api/send', {
+      from: {
+        name: EMAIL_FROM_NAME,
+        email: '',
+      },
+      to: options.to,
+      subject: options.subject,
+      html: options.html,
+      replyTo: EMAIL_REPLY_TO,
+      queue: EMAIL_QUEUE,
+    });
   } catch (error) {
     console.error(`Failed to send email to ${options.to}`, error);
   }
 }
 
 type BatchSendEmailOptions = Omit<SendMailOptions, 'to'> & { to: string[] };
-
 export async function batchSendEmail(options: BatchSendEmailOptions) {
   const { to, ...rest } = options;
-  for (const email of to) {
-    await singleSendEmail({ ...rest, to: email });
+  try {
+    await mailingAxios.post('/api/send-bulk', {
+      messages: to.map((email) => ({
+        from: {
+          name: EMAIL_FROM_NAME,
+          email: '',
+        },
+        to: email,
+        subject: rest.subject,
+        html: rest.html,
+        replyTo: EMAIL_REPLY_TO,
+      })),
+      queue: EMAIL_QUEUE,
+    });
+  } catch (error) {
+    console.error(`Failed to send email to ${to.length} recipients`, error);
   }
 }
