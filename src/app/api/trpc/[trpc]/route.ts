3014f5bbbd401f065440a6a16ce2499ca06dbfab
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-10-13 22:09:50 +0200

    fix: internal server error wrapping

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/node';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 
 import { appRouter } from '@/trpc';
@@ -9,6 +10,13 @@ const handler = (req: Request) =>
     req,
     router: appRouter,
     createContext,
+    onError: (opts) => {
+      if (opts.error.code === 'INTERNAL_SERVER_ERROR') {
+        Sentry.captureEvent({ ...opts.error });
+        opts.error.message = 'Nem várt szerver hiba';
+      }
+      return opts;
+    },
   });
 
 export { handler as GET, handler as POST };
