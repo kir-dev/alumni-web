5c8cd3a602f4b7f03024fc1e28efee4a27a04b6c
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-10-14 20:05:54 +0200

    fix: membership counter wrapping

--- src/components/group/membership-counter.tsx
+++ src/components/group/membership-counter.tsx
@@ -10,7 +10,7 @@ interface MembershipCounterProps {
 export default function MembershipCounter({ approvedCount, pendingCount }: MembershipCounterProps) {
   return (
     <InlineTooltip content='Jóváhagyott tagok és várakozók száma'>
-      <div className='flex-row justify-between items-center text-sm text-slate-500 border border-slate-500 bg-slate-500/10 px-2.5 py-1 rounded-full w-fit inline'>
+      <div className='flex-row justify-between items-center text-sm text-slate-500 border border-slate-500 bg-slate-500/10 px-2.5 py-1 rounded-full w-fit inline whitespace-nowrap'>
         <TbUserCheck className='text-green-500 inline' /> {approvedCount}
         {typeof pendingCount === 'number' ? (
           <>
