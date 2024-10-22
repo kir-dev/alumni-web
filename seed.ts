a0d4ed5040256cac663e8c2f386812afa00f87a8
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-10-22 22:15:41 +0200

    feat: seed script

---
+++ seed.ts
@@ -0,0 +1,61 @@
+import { faker } from '@faker-js/faker/locale/hu';
+import { Group, PrismaClient, User } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+type CreateGroup = Omit<Group, 'createdAt' | 'updatedAt' | 'id' | 'icon' | 'color'>;
+
+async function run() {
+  const users: Omit<User, 'createdAt' | 'updatedAt' | 'emailVerified' | 'id'>[] = [];
+  for (let i = 0; i < 50; i++) {
+    users.push({
+      address: faker.location.streetAddress(),
+      isSuperAdmin: false,
+      nickname: '',
+      password: faker.internet.password(),
+      email: faker.internet.email(),
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      phone: faker.phone.number(),
+    });
+  }
+  await prisma.user.createMany({ data: users });
+
+  const schId = await prisma.group.create({
+    data: {
+      name: 'Schönherz Alumni',
+      description: 'Root group',
+    },
+  });
+
+  const vikId = await prisma.group.create({
+    data: {
+      name: 'BME Villamosmérnöki és Informatikai Kar',
+      description: 'Root group',
+    },
+  });
+
+  const groups: CreateGroup[] = [];
+  for (let i = 0; i < 5; i++) {
+    groups.push({
+      name: faker.commerce.department(),
+      description: faker.lorem.sentence(),
+      parentGroupId: schId.id,
+    });
+    groups.push({
+      name: faker.commerce.department(),
+      description: faker.lorem.sentence(),
+      parentGroupId: vikId.id,
+    });
+  }
+  await prisma.group.createMany({ data: groups });
+}
+
+run()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
