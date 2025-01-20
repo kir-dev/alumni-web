1c4c5ff827e5cb0614678ca421dfc85470cde57b
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2025-01-20 20:17:13 +0100

    fix: ai sdk

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,25 +1,16 @@
 import { openai } from '@ai-sdk/openai';
-import { StreamData, StreamingTextResponse, streamText } from 'ai';
+import { streamText } from 'ai';
 
 export async function POST(req: Request) {
   const { messages, ...body } = await req.json();
 
   const context = body.context ?? '';
 
-  const result = await streamText({
+  const result = streamText({
     model: openai('gpt-3.5-turbo'),
     system: `Schönherz Alumni weboldalon szeretnénk szöveges tartalmat generálni. A tartalom alumnusoknak szóljon barátságos, de kissé hivatalos, tisztelettudú hangnemben, informatívan, egyértelműen. Az alábbi üzeneteket használd a generáláshoz követve az elején megjelenő formátumot! ${context}`,
     messages,
   });
 
-  const data = new StreamData();
-
-  const stream = result.toAIStream({
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    onFinal(_) {
-      data.close();
-    },
-  });
-
-  return new StreamingTextResponse(stream, {}, data);
+  return result.toDataStreamResponse();
 }
