b4dafd94d16f58c22a21a31c3d06dee1f8105978
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2025-02-08 18:45:04 +0100

    feat: show & confirm password

--- src/components/register/register-form.tsx
+++ src/components/register/register-form.tsx
@@ -6,10 +6,11 @@ import { useRouter } from 'next/navigation';
 import { signIn } from 'next-auth/react';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { TbEye, TbEyeOff } from 'react-icons/tb';
 import { z } from 'zod';
 
 import { trpc } from '@/_trpc/client';
-import { LoadingButton } from '@/components/ui/button';
+import { Button, LoadingButton } from '@/components/ui/button';
 import { CheckboxField, DateField, TextField } from '@/components/ui/fields';
 import { Form, FormMessage } from '@/components/ui/form';
 import { RegisterDto } from '@/types/user.types';
@@ -17,11 +18,21 @@ import { RegisterDto } from '@/types/user.types';
 const RegisterFormSchema = RegisterDto.merge(
   z.object({
     gdpr: z.boolean().refine((value) => value, { message: 'Az adatvédelmi irányelvek elfogadása kötelező' }),
+    confirmPassword: z.string().min(8, { message: 'A jelszónak legalább 8 karakter hosszúnak kell lennie' }),
   })
-);
+).superRefine((data, ctx) => {
+  if (data.password !== data.confirmPassword) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: 'A jelszavaknak egyeznie kell',
+      path: ['confirmPassword'],
+    });
+  }
+});
 
 export function RegisterForm() {
   const [loginLoading, setLoginLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
   const { mutateAsync, isPending, isError, error } = trpc.registerUser.useMutation();
 
@@ -31,15 +42,17 @@ export function RegisterForm() {
       firstName: '',
       email: '',
       password: '',
+      confirmPassword: '',
       phone: '',
       address: '',
+      graduationDate: null,
       gdpr: false,
     },
     resolver: zodResolver(RegisterFormSchema),
   });
 
-  const onSubmit = form.handleSubmit(async ({ gdpr, ...data }) => {
-    if (!gdpr) return;
+  const onSubmit = form.handleSubmit(async ({ gdpr, confirmPassword, ...data }) => {
+    if (!gdpr || confirmPassword !== data.password) return;
     setLoginLoading(true);
     await mutateAsync(data);
     const signInResponse = await signIn('credentials', {
@@ -63,7 +76,11 @@ export function RegisterForm() {
         <TextField control={form.control} type='text' name='firstName' label='Keresztnév' />
         <TextField control={form.control} type='text' name='nickname' label='Becenév (nem kötelező)' />
         <TextField control={form.control} type='email' name='email' label='Email' />
-        <TextField control={form.control} type='password' name='password' label='Jelszó' />
+        <TextField control={form.control} type={showPassword ? 'text' : 'password'} name='password' label='Jelszó' />
+        <Button type='button' variant='link' onClick={() => setShowPassword(!showPassword)} className='px-0'>
+          {showPassword ? <TbEyeOff /> : <TbEye />} Jelszó {showPassword ? 'elrejtése' : 'megjelenítése'}
+        </Button>
+        <TextField control={form.control} type='password' name='confirmPassword' label='Jelszó megerősítése' />
         <TextField control={form.control} type='tel' name='phone' label='Telefonszám' />
         <TextField control={form.control} type='text' name='address' label='Levelezési cím' />
         <DateField control={form.control} name='graduationDate' label='Felsőfokú végzettség dátuma' clearable />
